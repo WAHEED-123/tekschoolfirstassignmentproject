5d22400ce94309b0c54f63f16798ef1c0cec59c8
waheed <waheednushin123@gmail.com>
2022-01-27 18:28:43 -0800

    this assignment

---
+++ com.tekschool.asignment/src/main/java/com/tekschool/asignment/com/tekschool/asignment/App.java
@@ -0,0 +1,13 @@
+package com.tekschool.asignment.com.tekschool.asignment;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
